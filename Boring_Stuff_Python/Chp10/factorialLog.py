b20b1b45c117d0f31454d48d55ab79e3178c7131
light rao <lightrao@hotmail.com>
2018-05-16 08:17:56 +0800

    v

--- Boring_Stuff_Python/Chp10/factorialLog.py
+++ Boring_Stuff_Python/Chp10/factorialLog.py
@@ -4,7 +4,7 @@ logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - 
 logging.debug('Start of program')
 
 def factorial(n):
-    logging.debug('Start of factorial(%s)' % (n))
+    logging.debug('Start of factorial(%s)' % n)
     total = 1
     for i in range(1, n + 1):
         total *= i
@@ -12,6 +12,6 @@ def factorial(n):
     logging.debug('End of factorial(%s)' % (n))
     return total
 
-print(factorial(599))
+print(factorial(5))
 logging.debug('End of program')
 
